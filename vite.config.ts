79b337cf316a8e37421a5bfd5a4851427789f0bc
youngchan Kim <aacz12035@gmail.com>
2024-01-31 20:10:55 +0900

    chore: emotion 환경 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      plugins: [['@swc/plugin-emotion', {}]],
+    }),
+  ],
   resolve: {
     alias: [{ find: '~', replacement: '/src' }],
   },
