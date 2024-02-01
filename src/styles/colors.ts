12913f230addfa13d9c2ef021b66ce7a8b6c8e75
youngchan Kim <aacz12035@gmail.com>
2024-02-02 02:33:17 +0900

    feat: 프로젝트 color 추가

---
+++ src/styles/colors.ts
@@ -0,0 +1,22 @@
+export const colors = {
+  White: '#FFFFFF',
+
+  Gray000: '#F5F5F5',
+  Gray50: '#F2F2F2',
+  Gray100: '#D9D9D9',
+  Gray200: '#B6B6B6',
+  Gray300: '#959595',
+  Gray400: '#656D78',
+  Gray500: '#4A4A4A',
+
+  Primary100: '#EAF7FF',
+  Primary200: '#AFE0ED',
+  Primary300: '#4EB1CB',
+  Primary400: '#63A5BB',
+  Primary500: '#1E8FAE',
+
+  Red100: '#FFECEF',
+  Red200: '#F47F88',
+
+  Blue: '#005E9E',
+};
