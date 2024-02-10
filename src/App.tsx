86bd5ed3d00f38adff2bd0c1d5d451e4af2ddf71
youngchan Kim <aacz12035@gmail.com>
2024-02-10 16:18:52 +0900

    feat: BrowserRouter 구조 제작

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 import GlobalStyle from '~/styles/GlobalStyle';
 import { css } from '@emotion/react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import commonRoutes from '~/routes/commonRoutes';
+
+const getRoutes = (routes: any) => {
+  return routes.map((route: any, i: any) => (
+    <Route key={route.path} path={route.path} element={route.element}>
+      {route.children && getRoutes(route.children)}
+    </Route>
+  ));
+};
+const Router = () => {
+  return <Routes>{getRoutes(commonRoutes)}</Routes>;
+};
 
 function App() {
   return (
@@ -11,7 +24,9 @@ function App() {
             height: 100dvh;
             background-color: white;
           `}>
-          MainPage
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
         </div>
       </GlobalStyle>
     </>
