3947c3f60c1474cd09970a756ffebdd10ebf3132
이소은 <soeun@MacBook-Air.local>
2024-02-12 04:40:23 +0900

    feat: 버튼 커스텀 훅에 최대 선택 기능 추가

--- src/hooks/useToggleSelect.tsx
+++ src/hooks/useToggleSelect.tsx
@@ -1,13 +1,16 @@
 import { useState } from 'react';
 
-export function useToggleSelect<T>() {
+export function useToggleSelect<T>(maxSelection: number) {
   const [selectedValues, setSelectedValues] = useState<T[]>([]);
 
   const select = (value: T) => () => {
     if (selectedValues.includes(value)) {
       return setSelectedValues(selectedValues.filter(v => v !== value));
     }
-    setSelectedValues([...selectedValues, value]);
+    /* 최대 선택 개수를 제한하는 함수 추가 */
+    if (selectedValues.length < maxSelection) {
+      setSelectedValues([...selectedValues, value]);
+    }
   };
 
   const checkSelectedValues = (value: T) => {
