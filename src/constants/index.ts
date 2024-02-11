0631646cf42b3770183917b92d7f1d60f07f350c
이소은 <soeun@MacBook-Air.local>
2024-02-12 04:38:43 +0900

    feat: constants/index 파일에 동물 데이터 추가

---
+++ src/constants/index.ts
@@ -0,0 +1,25 @@
+import { animalKeyType } from '~/types/icon.type';
+
+export const ANIMALS: animalKeyType[] = [
+  'dog',
+  'cat',
+  'rabbit',
+  'fox',
+  'bear',
+  'hamster',
+  'monkey',
+  'dinosaur',
+  'chick',
+];
+
+export enum ANIMALS_NAME {
+  'dog' = '강아지',
+  'cat' = '고양이',
+  'rabbit' = '토끼',
+  'fox' = '여우',
+  'bear' = '곰',
+  'hamster' = '햄스터',
+  'monkey' = '원숭이',
+  'dinosaur' = '공룡',
+  'chick' = '병아리',
+}
