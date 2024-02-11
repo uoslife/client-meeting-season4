4a02f039537c71b7a045fe9b3f2d26b339ee3470
이소은 <soeun@MacBook-Air.local>
2024-02-12 06:10:23 +0900

    feat: TextInput에 xButton 아이콘 추가

---
+++ src/components/inputs/TextInput.tsx
@@ -0,0 +1,46 @@
+import { ChangeEvent } from 'react';
+import * as S from './TextInput.style';
+import IconButton from '../buttons/iconButton/IconButton';
+
+export type InputProps = {
+  type?: string;
+  value: string;
+  status: 'default' | 'focused' | 'filled' | 'error';
+  placeholder?: string;
+  onClick?: () => void;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  isAuthentication?: boolean;
+};
+
+const TextInput = ({
+  type = 'text',
+  value,
+  status,
+  placeholder,
+  onClick,
+  onChange,
+  isAuthentication,
+}: InputProps) => {
+  return (
+    <S.Container status={status}>
+      <S.Input
+        type={type}
+        value={value}
+        status={status}
+        placeholder={placeholder}
+        onChange={onChange}
+      />
+      <S.Icon status={status} isAuthentication={isAuthentication}>
+        <IconButton
+          iconName="xButton"
+          width={20}
+          height={20}
+          format="svg"
+          onClick={onClick}
+        />
+      </S.Icon>
+    </S.Container>
+  );
+};
+
+export default TextInput;
--- src/types/icon.type.ts
+++ src/types/icon.type.ts
@@ -1,6 +1,6 @@
 // 동물상이나 관심사처럼 많은 데이터를 type으로 지정할 필요가 없는 경우 이곳에 입력합니다.
 // 예를 들어, Header의 Home 아이콘, 랜딩 페이지의 카카오톡 아이콘 등.
-export type normalType = 'vite';
+export type normalType = 'vite' | 'xButton';
 
 // 동물상 관련 타입 지정입니다. 'dog' | 'cat' 이런 식으로 작성해주시면 됩니다.
 export type animalKeyType = '';
