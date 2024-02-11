5e48f24d729f802b254f6bb5dbc99adf0367cf92
limeojin363 <2002eojin@gmail.com>
2024-02-11 21:40:04 +0900

    feat: Card 및 BodyContainer(미완) 컴포넌트 작성

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled';
+import { colors } from '~/styles/colors';
+import { colorType } from '~/types/style.type';
+
+export type CardStyleProps = {
+  backgroundColor: colorType;
+  borderColor: colorType;
+  borderWidth: number;
+  borderRadius: number;
+};
+
+type CardProps = {
+  children: React.ReactNode;
+} & CardStyleProps;
+
+const Card = ({ children, ...styleProps }: CardProps) => {
+  return <S.Wrapper {...styleProps}>{children}</S.Wrapper>;
+};
+
+export default Card;
+
+const S = {
+  Wrapper: styled.div<CardStyleProps>`
+    border: solid ${({ borderColor }) => colors[borderColor]};
+    border-radius: ${({ borderRadius }) => borderRadius}px;
+    border-width: ${({ borderWidth }) => borderWidth}px;
+    height: 400px;
+
+    background-color: ${({ backgroundColor }) => colors[backgroundColor]};
+    width: 100%;
+  `,
+};
---
+++ src/components/layout/BodyContainer.tsx
@@ -0,0 +1,48 @@
+import styled from '@emotion/styled';
+import Card, { CardStyleProps } from '../card/Card';
+
+type BodyCardType = 'DEFAULT' | 'BG_GREY' | 'SPLITTED';
+
+type BodyContainerType = {
+  cardType: BodyCardType;
+  children: React.ReactNode;
+};
+
+const SelectedCard = ({ children, cardType }: BodyContainerType) => {
+  // TODO: 필요한 컴포넌트 제작 후에 SPLITTED case 대응
+  if (cardType === 'SPLITTED') return <>{children}</>;
+
+  const isDefault = cardType === 'DEFAULT';
+  const cardStyleProps: CardStyleProps = {
+    backgroundColor: isDefault ? 'White' : 'Gray000',
+    borderWidth: isDefault ? 2 : 1,
+    borderColor: 'Gray500',
+    borderRadius: 21,
+  };
+
+  return <Card {...cardStyleProps}>{children}</Card>;
+};
+
+const BodyContainer = ({ children, cardType }: BodyContainerType) => {
+  return (
+    <S.DashedBodyWrapper>
+      <S.CardWrapper>
+        <SelectedCard cardType={cardType}>{children}</SelectedCard>
+      </S.CardWrapper>
+    </S.DashedBodyWrapper>
+  );
+};
+
+export default BodyContainer;
+
+const S = {
+  DashedBodyWrapper: styled.div`
+    border-image-slice: 15;
+    border-image-width: 18px;
+    border-image-repeat: repeat;
+    border-image-source: url('/images/dashed-line-bg-white.png');
+  `,
+  CardWrapper: styled.div`
+    padding: 5px;
+  `,
+};
