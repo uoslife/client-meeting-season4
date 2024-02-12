2d62f0b18418f39be9c07f00acd16c406f069f26
youngchan Kim <aacz12035@gmail.com>
2024-02-13 03:34:17 +0900

    feat: Footer 컴포넌트 제작

---
+++ src/components/layout/footer/Footer.tsx
@@ -0,0 +1,41 @@
+import IconButton from '~/components/buttons/iconButton/IconButton';
+import Row from '~/components/layout/Row';
+import Text from '~/components/typography/Text';
+
+export type FooterPropsType = {
+  totalPage: number;
+  currentPage: number;
+  onPrev: () => void;
+  onNext: () => void;
+  isDisabled?: boolean;
+};
+
+const Footer = ({
+  totalPage,
+  currentPage,
+  onPrev,
+  onNext,
+  isDisabled,
+}: FooterPropsType) => {
+  return (
+    <Row justify={'space-between'} align={'center'}>
+      <IconButton
+        iconName={'footerButton-able'}
+        rotate={180}
+        onClick={onPrev}
+      />
+      <Text
+        label={`${currentPage} / ${totalPage}`}
+        color={'Gray300'}
+        typography={'PFLabelL'}
+      />
+      {isDisabled ? (
+        <IconButton iconName={'footerButton-disabled'} onClick={onNext} />
+      ) : (
+        <IconButton iconName={'footerButton-able'} onClick={onNext} />
+      )}
+    </Row>
+  );
+};
+
+export default Footer;
