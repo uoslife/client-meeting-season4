27a3e1cc3e32cb4f25e7cd84d8908e17f01e94a9
youngchan Kim <aacz12035@gmail.com>
2024-02-14 23:56:43 +0900

    feat: Header 컴포넌트 제작

---
+++ src/components/layout/header/Header.tsx
@@ -0,0 +1,55 @@
+import * as S from './Header.style';
+import Row from '~/components/layout/Row';
+import { useNavigate } from 'react-router-dom';
+import IconButton from '~/components/buttons/iconButton/IconButton';
+import Text from '~/components/typography/Text';
+import Pad from '~/components/layout/Pad';
+
+export type ProgressHeaderProps = {
+  title: string;
+  isBackArrow?: boolean;
+  isProgress?: boolean;
+  totalStep?: number;
+  currentStep?: number;
+};
+
+const Header = ({
+  title,
+  isBackArrow = false,
+  isProgress = false,
+  currentStep,
+  totalStep,
+}: ProgressHeaderProps) => {
+  const navigate = useNavigate();
+  return (
+    <S.Container isProgress={isProgress}>
+      <Pad left={5} right={5}>
+        <Row justify={'space-between'} align={'center'}>
+          <IconButton
+            iconName={
+              isBackArrow ? 'headerButton-backArrow' : 'headerButton-home'
+            }
+            width={24}
+            height={25.5}
+            onClick={() => navigate('/')}
+          />
+          <Text label={title} color={'White'} typography={'NeoTitleM'} />
+          <S.DummyBox />
+        </Row>
+      </Pad>
+
+      {isProgress && (
+        <S.ProgressContainer>
+          <S.ProgressBar size={(currentStep! / totalStep!) * 100} />
+          <Text
+            label={`${currentStep} / ${totalStep}`}
+            color={'Primary200'}
+            typography={'PFLabelM'}
+          />
+        </S.ProgressContainer>
+      )}
+    </S.Container>
+  );
+};
+
+export default Header;
