6811eca2c23df86a86aeacfb4ca234cff08252ae
youngchan Kim <aacz12035@gmail.com>
2024-02-14 23:59:08 +0900

    fix: 퍼널 예시 페이지 헤더 추가

--- src/pages/personal/myInformationStep/FirstPage.tsx
+++ src/pages/personal/myInformationStep/FirstPage.tsx
@@ -1,14 +1,7 @@
 import { PageProps } from '~/types/page.type';
-import { useEffect, useState } from 'react';
 
 const FirstPage = ({ setIsPageFinished }: PageProps) => {
-  const [abc, setAbc] = useState(false);
-
-  useEffect(() => {
-    abc ? setIsPageFinished(true) : setIsPageFinished(false);
-  }, [abc, setAbc]);
-
-  return <div>dsds</div>;
+  return <div>첫 번째 페이지</div>;
 };
 
 export default FirstPage;
