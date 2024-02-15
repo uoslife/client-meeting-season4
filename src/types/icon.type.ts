d17360aeaa4e71ccf937ea087b4e620825ec0bc7
이소은 <soeun@MacBook-Air.local>
2024-02-15 21:34:19 +0900

    feat: InterestButton 컴포넌트 제작

--- src/types/icon.type.ts
+++ src/types/icon.type.ts
@@ -1,6 +1,11 @@
 // 동물상이나 관심사처럼 많은 데이터를 type으로 지정할 필요가 없는 경우 이곳에 입력합니다.
 // 예를 들어, Header의 Home 아이콘, 랜딩 페이지의 카카오톡 아이콘 등.
-export type normalType = 'vite' | 'clearButton' | 'footerButton-able' | 'footerButton-disabled' | 'check';
+export type normalType =
+  | 'vite'
+  | 'clearButton'
+  | 'footerButton-able'
+  | 'footerButton-disabled'
+  | 'check';
 
 // 동물상 관련 타입 지정입니다. 'dog' | 'cat' 이런 식으로 작성해주시면 됩니다.
 export type animalKeyType =
@@ -16,7 +21,17 @@ export type animalKeyType =
 export type animalType = `animals/${animalKeyType}`;
 
 // 관심사 관련 타입 지정입니다. 'book' | 'game' 이런 식으로 작성해주시면 됩니다.
-export type interestKeyType = '';
+export type interestKeyType =
+  | 'reading'
+  | 'game'
+  | 'exercise'
+  | 'traveling'
+  | 'animal'
+  | 'music'
+  | 'drawing'
+  | 'movie_drama'
+  | 'fashion'
+  | 'cooking';
 export type interestType = `interests/${interestKeyType}`;
 
 export type iconType = normalType | animalType | interestType;
