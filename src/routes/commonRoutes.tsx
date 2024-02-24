9450b590e229bea740a184450aa1fd5074be086a
youngchan Kim <aacz12035@gmail.com>
2024-02-24 19:29:15 +0900

    fix: privacyPolicy 파일명 변경

--- src/routes/commonRoutes.tsx
+++ src/routes/commonRoutes.tsx
@@ -1,7 +1,7 @@
 import UnivVerificationStep from '~/pages/common/univVerificationStep/Step';
 import BranchGatewayStep from '~/pages/common/branchGatewayStep/Step';
 import PaymentStep from '~/pages/common/paymentStep/Step';
-import PrivacyPolicyStep from '~/pages/common/privacyPolicyStep /Step';
+import PrivacyPolicyStep from '~/pages/common/privacyPolicyStep/Step';
 import FinishApplyStep from '~/pages/common/finishApplyStep/Step';
 import CheckApplyInfoStep from '~/pages/common/checkApplyInfoStep/Step';
 import LandingStep from '~/pages/common/landingStep/Step';
