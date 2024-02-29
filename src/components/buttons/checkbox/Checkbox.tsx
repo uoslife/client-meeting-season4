a4b90368dd594dffc6bc2455f5eebdfe646355db
youngchan Kim <aacz12035@gmail.com>
2024-02-29 22:08:14 +0900

    feat: checkBox 컴포넌트 크기 props 추가

--- src/components/buttons/checkbox/Checkbox.tsx
+++ src/components/buttons/checkbox/Checkbox.tsx
@@ -3,11 +3,22 @@ import S from './Checkbox.style';
 export type CheckboxProps = {
   checked: boolean;
   onClick: () => void;
+  height?: number;
+  width?: number;
 };
 
-const Checkbox = ({ checked, onClick }: CheckboxProps) => {
+const Checkbox = ({
+  checked,
+  onClick,
+  height = 24,
+  width = 24,
+}: CheckboxProps) => {
   return (
-    <S.Wrapper checked={checked} onClick={onClick}>
+    <S.Wrapper
+      checked={checked}
+      onClick={onClick}
+      height={height}
+      width={width}>
       {/* TODO: img를 Icon 컴포넌트로 교체 */}
       {checked && <img src="/images/icons/checkbox-check.png" width={20} />}
     </S.Wrapper>
