117fc3d55d48237eb6a15966591eb1b7102b92b4
youngchan Kim <aacz12035@gmail.com>
2024-03-04 02:27:56 +0900

    feat: bottom sheet 모달 추가

--- src/pages/group/groupParticipateStep/FirstPage.tsx
+++ src/pages/group/groupParticipateStep/FirstPage.tsx
@@ -5,11 +5,13 @@ import styled from '@emotion/styled';
 import { useEffect, useRef, useState } from 'react';
 import { colors } from '~/styles/colors';
 import { typographies } from '~/styles/typographies';
+import ParticipationModal from '~/components/modal/participationModal/ParticipationModal';
 
 const FirstPage = () => {
   const inputRef = useRef<HTMLInputElement | null>(null);
   const [code, setCode] = useState('');
   const [isError, setIsError] = useState(false);
+  const [isModal, setIsModal] = useState(false);
 
   const handleInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
     const inputValue = e.target.value;
@@ -26,43 +28,57 @@ const FirstPage = () => {
     !!input && input.focus();
   }, [inputRef]);
 
+  useEffect(() => {
+    if (code.length === 4) setIsModal(true);
+  }, [code, setCode]);
+
   return (
-    <Col align={'center'} gap={20} padding={'36px 20px'}>
-      <Text
-        label={'팅을 만든 친구가 있나요?\n' + '공유받은 코드를 입력해 주세요'}
-        color={'Gray500'}
-        typography={'NeoTitleM'}
-        css={css`
-          text-align: center;
-        `}
-      />
-      <S.Container onClick={() => inputRef.current?.focus()}>
-        <S.Code isError={isError} active={!!code && code.length === 1}>
-          {code?.[0]}
-        </S.Code>
-        <S.Code isError={isError} active={!!code && code.length === 2}>
-          {code?.[1]}
-        </S.Code>
-        <S.Code isError={isError} active={!!code && code.length === 3}>
-          {code?.[2]}
-        </S.Code>
-        <S.Code isError={isError} active={!!code && code.length === 4}>
-          {code?.[3]}
-        </S.Code>
-        <S.Input
-          type="text"
-          maxLength={4}
-          ref={inputRef}
-          value={code}
-          onChange={handleInputValue}
+    <>
+      <Col align={'center'} gap={20} padding={'36px 20px'}>
+        <Text
+          label={'팅을 만든 친구가 있나요?\n' + '공유받은 코드를 입력해 주세요'}
+          color={'Gray500'}
+          typography={'NeoTitleM'}
+          css={css`
+            text-align: center;
+          `}
         />
-      </S.Container>
-      <Text
-        label={showStatusMessage()}
-        color={isError ? 'Red200' : 'Primary500'}
-        typography={'NeoButtonS'}
+        <S.Container onClick={() => inputRef.current?.focus()}>
+          <S.Code isError={isError} active={!!code && code.length === 1}>
+            {code?.[0]}
+          </S.Code>
+          <S.Code isError={isError} active={!!code && code.length === 2}>
+            {code?.[1]}
+          </S.Code>
+          <S.Code isError={isError} active={!!code && code.length === 3}>
+            {code?.[2]}
+          </S.Code>
+          <S.Code isError={isError} active={!!code && code.length === 4}>
+            {code?.[3]}
+          </S.Code>
+          <S.Input
+            type="text"
+            maxLength={4}
+            ref={inputRef}
+            value={code}
+            onChange={handleInputValue}
+          />
+        </S.Container>
+        <Text
+          label={showStatusMessage()}
+          color={isError ? 'Red200' : 'Primary500'}
+          typography={'NeoButtonS'}
+        />
+      </Col>
+      <ParticipationModal
+        isActive={isModal}
+        label={'asd'}
+        currentParticipant={1}
+        maxParticipant={3}
+        cancelButtonClicked={() => setIsModal(false)}
+        joinButtonClicked={() => setIsModal(false)}
       />
-    </Col>
+    </>
   );
 };
 
