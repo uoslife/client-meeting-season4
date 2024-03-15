5b3db1d88ecbc28cc03f03428aa7499353a58e44
limeojin363 <2002eojin@gmail.com>
2024-03-15 09:25:56 +0900

    fix: solved infinite rerender issue by memoization

--- src/hooks/useFunnel.tsx
+++ src/hooks/useFunnel.tsx
@@ -4,6 +4,7 @@ import {
   ReactElement,
   ReactNode,
   useEffect,
+  useMemo,
   useState,
 } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -45,58 +46,83 @@ export const useFunnel = ({
   const navigate = useNavigate();
   const LAST_PAGE = pageNumberList.length;
 
-  const FunnelComponent = ({ children }: FunnelProps) => {
-    // children 검증
-    const validChildren = Children.toArray(children)
-      // children이 ReactElement인지
-      .filter(isValidElement)
-      // pageNumber props가 pageNumberList에 있는지
-      .filter((childPage: ReactElement) =>
-        pageNumberList.includes(childPage.props.pageNumber),
-      );
+  const FunnelComponent = useMemo(
+    () =>
+      ({ children }: FunnelProps) => {
+        // children 검증
+        const validChildren = Children.toArray(children)
+          // children이 ReactElement인지
+          .filter(isValidElement)
+          // pageNumber props가 pageNumberList에 있는지
+          .filter((childPage: ReactElement) =>
+            pageNumberList.includes(childPage.props.pageNumber),
+          );
 
-    // currenPage state에 따라 현재 Page.
-    const targetPage = validChildren.find(
-      (child: ReactElement) => child.props.pageNumber === currentPage,
-    );
+        // currenPage state에 따라 현재 Page.
+        const targetPage = validChildren.find(
+          (child: ReactElement) => child.props.pageNumber === currentPage,
+        );
 
-    return <>{targetPage}</>;
-  };
+        return <>{targetPage}</>;
+      },
+    [currentPage, pageNumberList],
+  );
 
-  const PageComponent = ({ children }: PageProps) => {
-    return <>{children}</>;
-  };
+  const PageComponent = useMemo(
+    () =>
+      ({ children }: PageProps) => {
+        return <>{children}</>;
+      },
+    [],
+  );
 
-  const Funnel = Object.assign(FunnelComponent, {
-    Page: PageComponent,
-  });
+  const Funnel = useMemo(
+    () =>
+      Object.assign(FunnelComponent, {
+        Page: PageComponent,
+      }),
+    [FunnelComponent, PageComponent],
+  );
 
   //Footer 컴포넌트 버튼 로직
-  const PageHandler = {
-    // 앞으로 가기
-    onNext: () => {
-      setIsPageFinished(false);
-      if (currentPage === LAST_PAGE) {
-        setNavigateNextStep(true);
-        navigate(nextPath, { state: nextState });
-        return;
-      }
-      setCurrentPage(currentPage + 1);
-    },
-    // 뒤로 가기
-    onPrev: () => {
-      if (currentPage === 1) {
-        setNavigateNextStep(false);
-        navigate(prevPath, { state: prevState });
-        return;
-      }
-      setCurrentPage(currentPage - 1);
-    },
-  };
+  const PageHandler = useMemo(
+    () => ({
+      // 앞으로 가기
+      onNext: () => {
+        setIsPageFinished(false);
+        if (currentPage === LAST_PAGE) {
+          setNavigateNextStep(true);
+          navigate(nextPath, { state: nextState });
+          return;
+        }
+        setCurrentPage(currentPage + 1);
+      },
+      // 뒤로 가기
+      onPrev: () => {
+        if (currentPage === 1) {
+          setNavigateNextStep(false);
+          navigate(prevPath, { state: prevState });
+          return;
+        }
+        setCurrentPage(currentPage - 1);
+      },
+    }),
+    [
+      currentPage,
+      LAST_PAGE,
+      setNavigateNextStep,
+      setIsPageFinished,
+      navigate,
+      nextPath,
+      nextState,
+      prevPath,
+      prevState,
+    ],
+  );
 
   useEffect(() => {
     navigateNextStep ? setCurrentPage(1) : setCurrentPage(LAST_PAGE);
-  }, [navigateNextStep, setNavigateNextStep]);
+  }, [setNavigateNextStep, navigateNextStep, LAST_PAGE]);
 
   return {
     Funnel,
