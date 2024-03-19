0bf1bc32b84c34f1a438a59a1ce79b2334ca4ba0
alicehrk <81912837+alicehrk@users.noreply.github.com>
2024-03-19 10:13:04 +0900

    feat: api 연결

--- src/pages/group/leader/preferStep/Step.tsx
+++ src/pages/group/leader/preferStep/Step.tsx
@@ -1,13 +1,42 @@
 import PageLayout from '~/components/layout/page/PageLayout';
 import FirstPage from './FirstPage';
 import { useFunnel } from '~/hooks/useFunnel';
+import { MeetingAPI } from '~/api';
+import { useAtomValue } from 'jotai';
+import { groupDataAtoms } from '~/models/group/data';
 
 const GroupLeaderPreferStep = () => {
   const { Funnel, currentPage, PageHandler } = useFunnel({
     pageNumberList: [1],
-    nextStep: { path: '/group/leader/groupInformationStep' },
-    prevStep: { path: '/group/leader/pledgeStep' },
+    prevStep: { path: '/group/leader/groupInformationStep' },
+    nextStep: { path: '/group/leader/pledgeStep' },
   });
+
+  const groupLeaderPreferValue = useAtomValue(
+    groupDataAtoms.groupLeaderPreferStep.page1,
+  );
+
+  const { ageRange, atmosphere, univs } = groupLeaderPreferValue;
+
+  let mood: 'ACTIVE' | 'CALM' | 'NOT_MATTER';
+  if (atmosphere === '활발한 편') mood = 'ACTIVE';
+  else if (atmosphere === '차분한 편') mood = 'CALM';
+  else mood = 'NOT_MATTER';
+
+  const updatePrefer = async () => {
+    const res = await MeetingAPI.updatePrefer('TRIPLE', true, {
+      ageMin: ageRange[0],
+      ageMax: ageRange[1],
+      university: univs,
+      mood: mood,
+    });
+    console.log(res);
+  };
+
+  const nextFunction = () => {
+    updatePrefer();
+    PageHandler.onNext();
+  };
 
   return (
     <PageLayout>
@@ -27,7 +56,7 @@ const GroupLeaderPreferStep = () => {
       <PageLayout.Footer
         currentPage={currentPage}
         totalPage={1}
-        onNext={PageHandler.onNext}
+        onNext={nextFunction}
         onPrev={PageHandler.onPrev}
       />
     </PageLayout>
