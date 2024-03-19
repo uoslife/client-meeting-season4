2067dc9866790436547a8ae36b40fa185499f440
limeojin363 <2002eojin@gmail.com>
2024-03-19 21:12:57 +0900

    delete: 불필요한 코드 삭제

--- src/pages/common/verifyForCheckAfterAleadyAppliedStep/Step.tsx
+++ src/pages/common/verifyForCheckAfterAleadyAppliedStep/Step.tsx
@@ -1,26 +1,13 @@
 import PageLayout from '~/components/layout/page/PageLayout';
-import { useFunnel } from '~/hooks/useFunnel';
 import FirstPage from './FirstPage';
 
 const CommonVerifyForCheckAfterAleadyAppliedStep = () => {
-  const { PageHandler } = useFunnel({
-    prevStep: { path: '/common/finishApplyStep' },
-    nextStep: { path: '/common/checkAfterAleadyAppliedStep' },
-    pageNumberList: [1],
-  });
-
   return (
     <PageLayout>
       <PageLayout.Header title="신청 정보 확인하기" />
       <PageLayout.SingleCardBody>
         <FirstPage />
       </PageLayout.SingleCardBody>
-      <PageLayout.Footer
-        currentPage={1}
-        onNext={PageHandler.onNext}
-        onPrev={PageHandler.onPrev}
-        totalPage={1}
-      />
     </PageLayout>
   );
 };
