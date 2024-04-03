24bbe308f7c749ded0b6b94551269ed2238ac159
youngchan Kim <aacz12035@gmail.com>
2024-04-04 03:06:01 +0900

    feat: proxy 제거

--- vite.config.ts
+++ vite.config.ts
@@ -12,12 +12,4 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '~', replacement: '/src' }],
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5173/',
-        changeOrigin: true,
-      },
-    },
-  },
 });
