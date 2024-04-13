92d58221b3983f3fbf559d20e1b3b62e46f33eb8
youngchan Kim <aacz12035@gmail.com>
2024-04-13 18:05:13 +0900

    feat: mkcert 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+// import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,7 @@ export default defineConfig({
       jsxImportSource: '@emotion/react',
       plugins: [['@swc/plugin-emotion', {}]],
     }),
+    // mkcert(),
   ],
   resolve: {
     alias: [{ find: '~', replacement: '/src' }],
