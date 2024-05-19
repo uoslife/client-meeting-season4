3cdc95f24f362afb348c32620fc95f4aff020ee0
limeojin363 <2002eojin@gmail.com>
2024-05-20 04:43:05 +0900

    feat: 디자인 및 애니메이션 개선

--- src/pages/common/roulette/components/Game.tsx
+++ src/pages/common/roulette/components/Game.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from 'react';
+import { forwardRef, useRef, useState } from 'react';
 import Text from '~/components/typography/Text';
 import S from './Game.style';
 import Col from '~/components/layout/Col';
+import { colors } from '~/styles/colors';
 
 const StartButton = ({
   onClick,
@@ -10,10 +11,12 @@ const StartButton = ({
   onClick: () => void;
   isSpinning: boolean;
 }) => (
-  <S.StartButtonContainer onClick={onClick}>
+  <S.StartButtonContainer
+    onClick={onClick}
+    bgColor={isSpinning ? 'gray' : colors.LightBlue}>
     <Text
       color="White"
-      label={isSpinning ? 'STOP' : 'START'}
+      label={isSpinning ? 'WAIT' : 'START'}
       typography="NeoButtonL"
     />
   </S.StartButtonContainer>
@@ -54,8 +57,8 @@ const RouletteSupport = () => (
 );
 
 // 룰렛 본체
-const RouletteBody = ({ rotate }: { rotate: number }) => (
-  <S.BodyWrapper rotate={rotate}>
+const RouletteBody = forwardRef<HTMLDivElement>((_, ref) => (
+  <S.BodyWrapper ref={ref}>
     <img
       width={304}
       height={304}
@@ -63,33 +66,68 @@ const RouletteBody = ({ rotate }: { rotate: number }) => (
       alt="룰렛 본체"
     />
   </S.BodyWrapper>
+));
+
+const COLA = '콜라';
+const SOJU = '소주';
+const BEAR = '맥주';
+
+const OPTIONS = [COLA, BEAR, COLA, BEAR, SOJU, COLA, BEAR, SOJU];
+const OPTION_LENGTH = OPTIONS.length;
+
+const DEG_PER_OPTION = 360 / OPTION_LENGTH;
+
+// 기본 5바퀴 + 단일 옵션 절반 각도
+const BASE_DEG = 360 * 5 + DEG_PER_OPTION / 2;
+
+const STOP_DEG_CANDIDATES = Array.from(
+  { length: OPTION_LENGTH },
+  (_, i) => DEG_PER_OPTION * i,
 );
 
-const UNIT_OF_ROTATION = 360 / 72;
+const SPINNING_TIME = 2000;
 
 const Game = () => {
   const [isSpinning, setIsSpinning] = useState(false);
   // 회전된 정도
-  const [rotate, setRotate] = useState(0);
 
-  useEffect(() => {
-    if (isSpinning) {
-      const spin = () => setRotate(prev => prev + UNIT_OF_ROTATION);
-      const intervalID = setInterval(spin, 2);
+  const bodyRef = useRef<HTMLDivElement>(null);
 
-      return () => clearInterval(intervalID);
-    }
-  }, [isSpinning, setRotate]);
+  const start = () => {
+    setIsSpinning(true);
+
+    const rIndex = Math.floor(Math.random() * OPTION_LENGTH);
+
+    const selectedOptionText = OPTIONS[rIndex];
+
+    const selectedStopDeg = STOP_DEG_CANDIDATES[rIndex];
+
+    const calculatedTotalSpinDeg = BASE_DEG + selectedStopDeg;
+
+    bodyRef.current?.animate(
+      [
+        { transform: `rotate(0deg)` },
+        { transform: `rotate(${calculatedTotalSpinDeg}deg)` },
+      ],
+      {
+        fill: 'forwards',
+        duration: SPINNING_TIME,
+        easing: 'ease-out',
+      },
+    );
+
+    setTimeout(() => {
+      setIsSpinning(false);
+      alert(selectedOptionText);
+    }, SPINNING_TIME);
+  };
 
   return (
     <Col align="center" gap={18}>
-      <StartButton
-        isSpinning={isSpinning}
-        onClick={() => setIsSpinning(prev => !prev)}
-      />
+      <StartButton isSpinning={isSpinning} onClick={start} />
       <S.RouletteContainer>
         <BottomArrow />
-        <RouletteBody rotate={rotate} />
+        <RouletteBody ref={bodyRef} />
         <RouletteSupport />
         <SideIrumae />
       </S.RouletteContainer>
