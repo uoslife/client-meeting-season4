4f339a68cf03d276bf88f72f4c639f953d6a11d2
youngchan Kim <aacz12035@gmail.com>
2024-05-21 20:27:05 +0900

    fix: qa 32차 수정

--- src/pages/common/landingStep/Step.tsx
+++ src/pages/common/landingStep/Step.tsx
@@ -234,20 +234,25 @@ const BottomCardComponent = () => {
         </Row>
       </Col>
       <Col gap={8} padding={'0 0 10px 0'}>
-        {isPaymentFinishedValue ? (
-          <RoundButton
-            status={'cancel'}
-            borderType={'black'}
-            label={'신청 정보 확인하기'}
-            onClick={handleOnClickSecondary}
-          />
-        ) : (
-          <RoundButton
-            status={'active'}
-            label={'신청하기'}
-            onClick={handleOnClickPrimary}
-          />
-        )}
+        {/*{isPaymentFinishedValue ? (*/}
+        {/*  <RoundButton*/}
+        {/*    status={'cancel'}*/}
+        {/*    borderType={'black'}*/}
+        {/*    label={'신청 정보 확인하기'}*/}
+        {/*    onClick={handleOnClickSecondary}*/}
+        {/*  />*/}
+        {/*) : (*/}
+        {/*  <RoundButton*/}
+        {/*    status={'active'}*/}
+        {/*    label={'신청하기'}*/}
+        {/*    onClick={handleOnClickPrimary}*/}
+        {/*  />*/}
+        {/*)}*/}
+        <RoundButton
+          status={'active'}
+          label={'서비스 점검중입니다.'}
+          onClick={() => toast.error('빠르게 고치고 있습니다.')}
+        />
       </Col>
       <Col align={'center'} gap={10}>
         <Text
