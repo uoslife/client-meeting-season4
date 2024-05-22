6163dc719df58687ae008f1b77d9ae8997ce5fef
youngchan Kim <aacz12035@gmail.com>
2024-05-22 20:08:41 +0900

    fix: 긴급 점검 버튼

--- src/pages/common/landingStep/Step.tsx
+++ src/pages/common/landingStep/Step.tsx
@@ -242,20 +242,27 @@ const BottomCardComponent = () => {
         </Row>
       </Col>
       <Col gap={8} padding={'0 0 10px 0'}>
-        {isPaymentFinishedValue ? (
-          <RoundButton
-            status={'cancel'}
-            borderType={'black'}
-            label={'신청 정보 확인하기'}
-            onClick={handleOnClickSecondary}
-          />
-        ) : (
-          <RoundButton
-            status={'active'}
-            label={'신청하기'}
-            onClick={handleOnClickPrimary}
-          />
-        )}
+        {/*{isPaymentFinishedValue ? (*/}
+        {/*  <RoundButton*/}
+        {/*    status={'cancel'}*/}
+        {/*    borderType={'black'}*/}
+        {/*    label={'신청 정보 확인하기'}*/}
+        {/*    onClick={handleOnClickSecondary}*/}
+        {/*  />*/}
+        {/*) : (*/}
+        {/*  <RoundButton*/}
+        {/*    status={'active'}*/}
+        {/*    label={'신청하기'}*/}
+        {/*    onClick={handleOnClickPrimary}*/}
+        {/*  />*/}
+        {/*)}*/}
+        <RoundButton
+          status={'cancel'}
+          label={'서비스 점검중입니다 '}
+          onClick={() =>
+            toast.error('곧 점검이 끝날 예정이니 잠시만 기다려주세요.')
+          }
+        />
       </Col>
       <Col align={'center'} gap={10}>
         <Text
